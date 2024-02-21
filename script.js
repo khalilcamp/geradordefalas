ed4e5b1b49f3a8c667d49555a0f72875e8ca6a93
Khalil Camargo Packer <kacamargopacker@gmail.com>
2024-02-21 18:39:40 -0300

    Um gerador de falas motivacionais

---
+++ script.js
@@ -0,0 +1,124 @@
+function generate(){
+    var quotes = {
+        "- Maya Angelou" : '“Você enfrentará muitas derrotas na vida, mas nunca se deixe ser derrotado.”',
+        "– Nelson Mandela": '“A maior glória de viver não está em nunca cair, mas em nos levantar toda vez que caímos.”',
+        "– Abraham Lincoln": ' “No final das contas, não são os anos de sua vida que contam. É a vida em seus anos.”'
+    }
+
+    var authors = Object.keys(quotes);
+
+    var author = authors[Math.floor(Math.random() * authors.length)];
+
+    var quote = quotes[author];
+
+    document.getElementById("quotes").innerHTML = quote;
+    document.getElementById("author").innerHTML = author;
+
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 80,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#535C91"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 5,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#535C91",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    });
+
+
+}
